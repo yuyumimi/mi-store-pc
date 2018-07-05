38ee3fef935a0ef210da61ad64cb1b8a7cdb8f77
xiatian <2698021598@git.cloud.tencent.com>
2018-07-05 19:54:14 +0800

    new component

---
+++ src/components/floor-product-item.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="floor-cell">
+    <img class="img" :src="imgSrc" alt="手机图片">
+    <a href="#">
+      <h5 class="title">{{title}}</h5>
+    </a>
+    <p class="desc">{{subtitle}}</p>
+    <p class="price">
+      <span>{{priceNow}}</span>
+    </p>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["title", "subtitle", "priceNow", "priceOri", "imgSrc"]
+}
+</script>
